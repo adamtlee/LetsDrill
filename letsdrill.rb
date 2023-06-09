700f7b84ebfaf3c2621bf4f5d0f1d8bca4d93e5e
adamtlee <adam.lee346@gmail.com>
2023-06-08 22:55:37 -0600

    solved challenges

--- letsdrill.rb
+++ letsdrill.rb
@@ -1,13 +1,45 @@
 def get_letter_grade(integer)
  
   #Put your code here!
+  if integer >= 90 && integer <= 100
+    grade = "A"
+    return grade
+  end
+  
+  if integer >= 80 && integer <= 89
+    grade = "B"
+    return grade
+  end
+  
+  if integer >= 70 && integer <= 79
+    grade = "C"
+    return grade
+  end
+  
+  if integer >= 60 && integer <= 69
+    grade = "D"
+    return grade
+  end
+  
+  if integer <= 59
+    grade = "F"
+    return grade
+  end
 
 end
 
 def shortest_string(array)
 
   #Put your code here!
-
+  shortest_string = nil
+  array.each do |item|
+    if item.is_a?(String)
+      if shortest_string.nil? || item.length < shortest_string.length
+        shortest_string = item
+      end
+    end
+  end
+  return shortest_string
 end
  
 
